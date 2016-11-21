5c15d6d4a3f89d93b0b1257e7754135f064f9f80
Trish Griego <pmgriego@gmail.com>
2016-11-21 14:23:50 -0800

    Set up code for working with clicking on the individual buttons. Sound is rendering but isn't allowing me to hit it more then one click

--- noise.js
+++ noise.js
@@ -1,3 +1,38 @@
 $(document).ready( function() {
   // your code here
+
+// target box button class C & aim
+$(".c").click(function(){
+  // alert("this worked!"); // this checkos to see if it will alert once clicked.
+
+  // now select your audio you wish to be played.
+  $('#cAudio').trigger('play');
+
 });
+
+//select d button
+$(".d").click(function() {
+  // play dAudio
+  $("#dAudio").trigger('play');
+});
+
+$(".e").click(function(){
+  $("#eAudio").trigger('play');
+});
+
+$(".f").click(function(){
+  $("#fAudio").trigger('play');
+});
+
+$(".a").click(function(){
+  $("#aAudio").trigger('play');
+});
+
+$(".b").click(function(){
+  $("#bAudio").trigger('play');
+});
+
+
+
+
+}); // end of code block
